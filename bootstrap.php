5df547df4124eb5e425e4cb7bff35e5ce33966c2
Bert Ramakers <bertramakers@me.com>
2015-05-29 16:33:45 +0200

    UBR-45: Use the UiTID provider library to authenticate users.

--- bootstrap.php
+++ bootstrap.php
@@ -14,4 +14,19 @@ $app->register(new \DerAlex\Silex\YamlConfigServiceProvider(__DIR__ . '/config.y
  */
 $app['debug'] = $app['config']['debug'] === true;
 
+/**
+ * Url generator.
+ */
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+/**
+ * Authentication services.
+ */
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new CultuurNet\UiTIDProvider\AuthServiceProvider($app['session']), array(
+    'uitid.base_url' => $app['config']['uitid']['base_url'],
+    'uitid.consumer.key' => $app['config']['uitid']['consumer']['key'],
+    'uitid.consumer.secret' => $app['config']['uitid']['consumer']['secret'],
+));
+
 return $app;
--- web/index.php
+++ web/index.php
@@ -1,9 +1,16 @@
 <?php
 
-$app = require_once __DIR__.'/../bootstrap.php';
+$app = require_once __DIR__ . '/../bootstrap.php';
 
-$app->get('/hello', function () {
-    return 'Hello!';
-});
+/**
+ * Authentication controllers.
+ */
+$app->mount('culturefeed/oauth', new \CultuurNet\UiTIDProvider\AuthControllerProvider(
+    $app['auth_service'],
+    $app['session'],
+    $app['url_generator']
+));
+
+
 
 $app->run();
