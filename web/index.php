1f861c4b9f500571b9b7e5a9a9291cecf9409504
Bert Ramakers <bertramakers@me.com>
2015-06-04 11:08:01 +0200

    UBR-45: Added controllers for UiTID user data.

--- web/index.php
+++ web/index.php
@@ -1,14 +1,43 @@
 <?php
 
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/* @var Application $app */
 $app = require_once __DIR__ . '/../bootstrap.php';
 
 /**
  * Authentication controllers.
  */
-$app->mount('culturefeed/oauth', new \CultuurNet\UiTIDProvider\Auth\AuthControllerProvider(
+$authController = new \CultuurNet\UiTIDProvider\Auth\AuthControllerProvider(
     $app['uitid_auth_service'],
     $app['session'],
     $app['url_generator']
-));
+);
+$app->mount('culturefeed/oauth', $authController);
+
+/**
+ * Authentication verification callback.
+ */
+$checkAuthentication = function(Request $request, Application $app) {
+    /* @var CultuurNet\UiTIDProvider\Session\UserSession $session */
+    $session = $app['session'];
+
+    if (is_null($session->getUser())) {
+        return new Response('Access denied.', 403);
+    } else {
+        return null;
+    }
+};
+
+/**
+ * API callbacks for UiTID user data and methods.
+ *
+ * @var \Silex\ControllerCollection $userControllers
+ */
+$userControllers = $app['controllers_factory'];
+$userControllers->before($checkAuthentication);
+$app->mount('uitid', new \CultuurNet\UiTIDProvider\UserControllerProvider($userControllers));
 
 $app->run();
