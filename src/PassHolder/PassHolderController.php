c9569d4a4e354884169de8f300568b1351b846e8
Bert Ramakers <bertramakers@me.com>
2015-06-26 19:52:18 +0200

    UBR-70: Added update() and getByUitpasNumber() to PassHolderController, with corresponding tests.

--- src/PassHolder/PassHolderController.php
+++ src/PassHolder/PassHolderController.php
@@ -2,6 +2,7 @@
 
 namespace CultuurNet\UiTPASBeheer\PassHolder;
 
+use CultuurNet\UiTPASBeheer\Exception\ReadableCodeResponseException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -34,5 +35,50 @@ class PassHolderController
         return JsonResponse::create()
             ->setData($passholder)
             ->setPrivate();
+    }
+
+    /**
+     * @param string $uitpasNumber
+     * @return JsonResponse
+     */
+    public function getByUitpasNumber($uitpasNumber)
+    {
+        $passholder = $this->passHolderService->getByUitpasNumber($uitpasNumber);
+
+        if (is_null($passholder)) {
+            throw new PassHolderNotFoundException();
+        }
+
+        return JsonResponse::create()
+            ->setData($passholder)
+            ->setPrivate();
+    }
+
+    /**
+     * @param Request $request
+     * @param string $uitpasNumber
+     *
+     * @return JsonResponse
+     *
+     * @throws ReadableCodeResponseException
+     *   When a CultureFeed_Exception is encountered.
+     */
+    public function update(Request $request, $uitpasNumber)
+    {
+        $passHolder = new \CultureFeed_Uitpas_Passholder();
+        $passHolder->uitpasNumber = $uitpasNumber;
+
+        $properties = $request->request->all();
+        foreach ($properties as $property => $value) {
+            $passHolder->{$property} = $value;
+        }
+
+        try {
+            $this->passHolderService->update($passHolder);
+        } catch (\CultureFeed_Exception $exception) {
+            throw ReadableCodeResponseException::fromCultureFeedException($exception);
+        }
+
+        return $this->getByUitpasNumber($uitpasNumber);
     }
 }
