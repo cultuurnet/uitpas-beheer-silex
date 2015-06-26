ba587ece591f87b2359f93f00e8a51450597c7c8
Bert Ramakers <bertramakers@me.com>
2015-06-26 19:49:53 +0200

    UBR-70: Added ReadableCodeResponseException, with tests.

---
+++ src/Exception/ReadableCodeResponseException.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace CultuurNet\UiTPASBeheer\Exception;
+
+class ReadableCodeResponseException extends ResponseException implements ReadableCodeExceptionInterface
+{
+    /**
+     * @var string
+     */
+    protected $readableCode;
+
+    /**
+     * @param string $message
+     * @param string $readableCode
+     * @param int $code
+     * @param \Exception $previous
+     */
+    public function __construct($message, $readableCode, $code = 400, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->setReadableCode($readableCode);
+    }
+
+    /**
+     * @param string $readableCode
+     */
+    public function setReadableCode($readableCode)
+    {
+        $this->readableCode = $readableCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReadableCode()
+    {
+        return $this->readableCode;
+    }
+
+    /**
+     * @param \CultureFeed_Exception $previous
+     * @param int $code
+     */
+    public static function fromCultureFeedException(\CultureFeed_Exception $exception, $code = 400)
+    {
+        $readableCode = $exception->error_code;
+
+        if ($readableCode === 'ACCESS_DENIED') {
+            $code = '403';
+        }
+
+        return new self($exception->getMessage(), $readableCode, $code, $exception);
+    }
+}
---
+++ test/Exception/ReadableCodeResponseExceptionTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace CultuurNet\UiTPASBeheer\Exception;
+
+class ReadableCodeResponseExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var string
+     */
+    protected $message;
+
+    /**
+     * @var string
+     */
+    protected $readableCode;
+
+    /**
+     * @var int
+     */
+    protected $code;
+
+    /**
+     * @var \CultureFeed_Exception
+     */
+    protected $cultureFeedException;
+
+    public function setUp()
+    {
+        $this->message = 'Something went wrong.';
+        $this->readableCode = 'SOMETHING_WRONG';
+        $this->code = 500;
+
+        $this->cultureFeedException = new \CultureFeed_Exception(
+            $this->message,
+            $this->readableCode
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_a_configurable_readable_code()
+    {
+        $exception = new ReadableCodeResponseException(
+            $this->message,
+            $this->readableCode,
+            $this->code
+        );
+
+        $this->assertEquals($this->readableCode, $exception->getReadableCode());
+
+        $exception->setReadableCode('OTHER_CODE');
+        $this->assertEquals('OTHER_CODE', $exception->getReadableCode());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_be_instantiated_from_a_culturefeed_exception()
+    {
+        $exception = ReadableCodeResponseException::fromCultureFeedException(
+            $this->cultureFeedException,
+            $this->code
+        );
+
+        $this->assertEquals($this->message, $exception->getMessage());
+        $this->assertEquals($this->readableCode, $exception->getReadableCode());
+        $this->assertEquals($this->code, $exception->getCode());
+
+        $accessDeniedException = new \CultureFeed_Exception('Access denied.', 'ACCESS_DENIED');
+        $exception = ReadableCodeResponseException::fromCultureFeedException(
+            $accessDeniedException
+        );
+
+        $this->assertEquals(403, $exception->getCode());
+    }
+}
