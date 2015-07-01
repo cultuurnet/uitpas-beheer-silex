0a8af6441549bef40850747691475ea4724a2b61
Bert Ramakers <bertramakers@me.com>
2015-07-01 15:15:32 +0200

    UBR-93: UiTPASNumber value object and test.

---
+++ src/UiTPAS/UiTPASNumber.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace CultuurNet\UiTPASBeheer\UiTPAS;
+
+use PlasmaConduit\Luhn;
+use ValueObjects\StringLiteral\StringLiteral;
+
+/**
+ * @see Functional description UiTPAS system.
+ */
+class UiTPASNumber extends StringLiteral
+{
+    /**
+     * An UiTPAS number is always exactly 13 digits and start with 0.
+     */
+    const FORMAT = '^0\d{12}$';
+
+    /**
+     * Contains prefix and serial number.
+     *
+     * We don't have a fixed list of prefixes and there's no separator.
+     * Should be a string because it should always start with a 0.
+     *
+     * @var string
+     */
+    protected $number;
+
+    /**
+     * @var int
+     */
+    protected $kansenStatuutDigit;
+
+    /**
+     * @var int
+     */
+    protected $checkDigit;
+
+    /**
+     * @param string $value
+     *
+     * @throws \InvalidArgumentException
+     *   When the provided value is not a string of exactly 13 digits.
+     *   When the provided value does not start with 0.
+     *   When the provided value is not valid according to the Luhn algorithm.
+     *   When the provided value's second to last digit (kansenstatuut) is not 0 or 1.
+     */
+    public function __construct($value)
+    {
+        // Validate length and contents of the string.
+        if (!preg_match('/' . self::FORMAT . '/', $value)) {
+            throw new \InvalidArgumentException('The provided value should be exactly 13 digits and start with 0.');
+        }
+
+        // Make sure the number is valid according to the Luhn algorithm.
+        if (!Luhn::validate($value)) {
+            throw new \InvalidArgumentException('The provided value does not have a valid check digit.');
+        }
+
+        // The number consists of all digits except for the last two.
+        $this->number = substr($value, 0, -2);
+
+        // Kansen statuut digit is the second last digit.
+        $this->kansenStatuutDigit = (int) substr($value, -2, 1);
+        if (!in_array($this->kansenStatuutDigit, [0, 1])) {
+            throw new \InvalidArgumentException('The second to last digit of the provided value should be 0 or 1.');
+        }
+
+        // Check digit is the last digit.
+        $this->checkDigit = (int) substr($value, -1);
+
+        // Finally store the value.
+        parent::__construct($value);
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasKansenStatuut()
+    {
+        return (bool) $this->kansenStatuutDigit;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCheckDigit()
+    {
+        return $this->checkDigit;
+    }
+}
---
+++ test/UiTPAS/UiTPASNumberTest.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace CultuurNet\UiTPASBeheer\UiTPAS;
+
+class UiTPASNumberTest extends \PHPUnit_Framework_TestCase
+{
+    const VALID_REGULAR = '0930000420206';
+    const VALID_KANSENSTATUUT = '0930000237915';
+
+    const INVALID_START_DIGIT = '1930000420206';
+    const INVALID_TOO_SHORT = '093000042020';
+    const INVALID_TOO_LONG = '09300004202066';
+    const INVALID_CHARACTERS = '09A3B004202066';
+    const INVALID_CHECK_DIGIT = '0930000237912';
+    const INVALID_KANSENSTATUUT = '0930000237931';
+
+    /**
+     * @test
+     */
+    public function it_can_return_the_number_without_kansenstatuut_and_check_digit()
+    {
+        $number = new UiTPASNumber(self::VALID_REGULAR);
+        $this->assertTrue(0 === strcmp('09300004202', $number->getNumber()));
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_return_kansenstatuut_status()
+    {
+        $regular = new UiTPASNumber(self::VALID_REGULAR);
+        $this->assertFalse($regular->hasKansenStatuut());
+
+        $kansenStatuut = new UiTPASNumber(self::VALID_KANSENSTATUUT);
+        $this->assertTrue($kansenStatuut->hasKansenStatuut());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_return_the_check_digit()
+    {
+        $number = new UiTPASNumber(self::VALID_REGULAR);
+        $this->assertTrue(6 === $number->getCheckDigit());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_cast_back_to_original_string()
+    {
+        $number = new UiTPASNumber(self::VALID_REGULAR);
+
+        $string = (string) $number;
+        $this->assertTrue(self::VALID_REGULAR === $string);
+
+        $native = $number->toNative();
+        $this->assertTrue(self::VALID_REGULAR === $native);
+    }
+
+    /**
+     * @test
+     */
+    public function it_validates_that_the_number_starts_with_zero()
+    {
+        $this->setExpectedException(
+            \InvalidArgumentException::class,
+            'The provided value should be exactly 13 digits and start with 0.'
+        );
+        new UiTPASNumber(self::INVALID_START_DIGIT);
+    }
+
+    /**
+     * @test
+     */
+    public function it_validates_that_the_number_is_not_shorter_than_13_digits()
+    {
+        $this->setExpectedException(
+            \InvalidArgumentException::class,
+            'The provided value should be exactly 13 digits and start with 0.'
+        );
+        new UiTPASNumber(self::INVALID_TOO_SHORT);
+    }
+
+    /**
+     * @test
+     */
+    public function it_validates_that_the_number_is_not_longer_than_13_digits()
+    {
+        $this->setExpectedException(
+            \InvalidArgumentException::class,
+            'The provided value should be exactly 13 digits and start with 0.'
+        );
+        new UiTPASNumber(self::INVALID_TOO_LONG);
+    }
+
+    /**
+     * @test
+     */
+    public function it_validates_that_the_number_contains_only_digits()
+    {
+        $this->setExpectedException(
+            \InvalidArgumentException::class,
+            'The provided value should be exactly 13 digits and start with 0.'
+        );
+        new UiTPASNumber(self::INVALID_CHARACTERS);
+    }
+
+    /**
+     * @test
+     */
+    public function it_validates_the_check_digit()
+    {
+        $this->setExpectedException(
+            \InvalidArgumentException::class,
+            'The provided value does not have a valid check digit.'
+        );
+        new UiTPASNumber(self::INVALID_CHECK_DIGIT);
+    }
+
+    /**
+     * @test
+     */
+    public function it_validates_the_kansenstatuut_digit()
+    {
+        $this->setExpectedException(
+            \InvalidArgumentException::class,
+            'The second to last digit of the provided value should be 0 or 1.'
+        );
+        new UiTPASNumber(self::INVALID_KANSENSTATUUT);
+    }
+}
