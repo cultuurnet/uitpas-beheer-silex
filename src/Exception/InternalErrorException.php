fbae952a6e2747c8f1d5b3f78fdc610fe55b74b8
Bert Ramakers <bertramakers@me.com>
2015-07-07 16:37:56 +0200

    UBR-72: Simplified InternalErrorException.

--- src/Exception/InternalErrorException.php
+++ src/Exception/InternalErrorException.php
@@ -2,18 +2,10 @@
 
 namespace CultuurNet\UiTPASBeheer\Exception;
 
-class InternalErrorException extends ResponseException implements ReadableCodeExceptionInterface
+class InternalErrorException extends ReadableCodeResponseException
 {
     public function __construct()
     {
-        parent::__construct('An internal error occurred.', 500);
-    }
-
-    /**
-     * @return string
-     */
-    public function getReadableCode()
-    {
-        return 'INTERNAL_ERROR';
+        parent::__construct('An internal error occurred.', 'INTERNAL_ERROR', 500);
     }
 }
