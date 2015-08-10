9da5f01d102a69991b6526f8eb0018c1467c145d
Bram Cordie <bramcordie@gmail.com>
2015-08-10 08:48:57 +0200

    UBR-74 Test Checkin controller error handling

--- test/Activity/CheckinControllerTest.php
+++ test/Activity/CheckinControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace CultuurNet\UiTPASBeheer\Activity;
 
+use CultuurNet\UiTPASBeheer\Exception\ReadableCodeResponseException;
 use CultuurNet\UiTPASBeheer\UiTPAS\UiTPASNumber;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -73,5 +74,45 @@ class CheckinControllerTest extends \PHPUnit_Framework_TestCase
             ->setPrivate();
 
         $this->assertEquals($expectedResponse, $updatedActivityResponse);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_a_readable_error_when_unable_to_find_an_activity()
+    {
+        $checkinData = '{"eventCdbid": "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee"}';
+        $request = new Request([], [], [], [], [], [], $checkinData);
+        $uitpasNumber = '0930000125607';
+
+        $this->activityService->expects($this->once())
+            ->method('get')
+            ->with(new UiTPASNumber($uitpasNumber), new Cdbid('aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee'))
+            ->willReturn(null);
+
+        $this->setExpectedException(ActivityNotFoundException::class);
+
+        $this->controller->checkin($request, $uitpasNumber);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_a_readable_error_when_unable_to_checkin_a_passholder()
+    {
+        $checkinData = '{"eventCdbid": "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee"}';
+        $request = new Request([], [], [], [], [], [], $checkinData);
+        $uitpasNumber = '0930000125607';
+
+        $exception = new \CultureFeed_Exception('error', 0);
+
+        $this->checkinService->expects($this->once())
+            ->method('checkin')
+            ->with(new UiTPASNumber($uitpasNumber), new Cdbid('aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee'))
+            ->willThrowException($exception);
+
+        $this->setExpectedException(ReadableCodeResponseException::class);
+
+        $this->controller->checkin($request, $uitpasNumber);
     }
 }
