d552f4f894e80d1d26b9daa67fb1c3c69e431203
Bert Ramakers <bertramakers@me.com>
2015-08-19 14:44:06 +0200

    UBR-135: Test for instantiating an Activity from a CultureFeed_Uitpas_Event_CultureEvent object.

--- test/Activity/ActivityTest.php
+++ test/Activity/ActivityTest.php
@@ -133,4 +133,58 @@ class ActivityTest extends \PHPUnit_Framework_TestCase
             'Activity/data/activity.json'
         );
     }
+
+    /**
+     * @test
+     */
+    public function it_can_be_instantiated_from_a_culturefeed_uitpas_event()
+    {
+        $cfEvent = new \CultureFeed_Uitpas_Event_CultureEvent();
+
+        $cfEvent->cdbid = $this->id->toNative();
+        $cfEvent->title = $this->title->toNative();
+        $cfEvent->checkinAllowed = $this->checkinConstraint->getAllowed();
+        $cfEvent->checkinStartDate = $this->checkinConstraint->getStartDate()->toNativeDateTime()->format('U');
+        $cfEvent->checkinEndDate = $this->checkinConstraint->getEndDate()->toNativeDateTime()->format('U');
+
+        $cfFirstPriceClass = new \CultureFeed_Uitpas_Event_PriceClass();
+        $cfFirstPriceClass->name = 'Rang 1';
+        $cfFirstPriceClass->price = 30;
+        $cfFirstPriceClass->tariff = 22;
+
+        $cfSecondPriceClass = new \CultureFeed_Uitpas_Event_PriceClass();
+        $cfSecondPriceClass->name = 'Rang 2';
+        $cfSecondPriceClass->price = 15;
+        $cfSecondPriceClass->tariff = 11;
+
+        $cfThirdPriceClass = new \CultureFeed_Uitpas_Event_PriceClass();
+        $cfThirdPriceClass->name = 'Rang 3+';
+        $cfThirdPriceClass->price = 7.5;
+        $cfThirdPriceClass->tariff = 5.5;
+
+        $cfKansentarief = new \CultureFeed_Uitpas_Event_TicketSale_Opportunity();
+        $cfKansentarief->type = \CultureFeed_Uitpas_Event_TicketSale_Opportunity::TYPE_DEFAULT;
+        $cfKansentarief->priceClasses = array(
+            $cfFirstPriceClass,
+            $cfSecondPriceClass,
+            $cfThirdPriceClass,
+        );
+
+        $cfTicketSaleCoupon = new \CultureFeed_Uitpas_Event_TicketSale_Coupon();
+        $cfTicketSaleCoupon->name = 'Cultuurwaardebon';
+
+        $cfCoupon = new \CultureFeed_Uitpas_Event_TicketSale_Opportunity();
+        $cfCoupon->type = \CultureFeed_Uitpas_Event_TicketSale_Opportunity::TYPE_COUPON;
+        $cfCoupon->priceClasses = array(
+            $cfFirstPriceClass,
+            $cfSecondPriceClass,
+            $cfThirdPriceClass,
+        );
+        $cfCoupon->ticketSaleCoupon = $cfTicketSaleCoupon;
+
+        $cfEvent->ticketSales = array(
+            $cfKansentarief,
+            $cfCoupon,
+        );
+    }
 }
