185f7d1c633ce9a263b951b48a74f0e77538974e
Bert Ramakers <bertramakers@me.com>
2015-10-07 13:20:28 +0200

    UBR-291: ExpenseReportService and corresponding tests.

---
+++ src/ExpenseReport/ExpenseReportService.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace CultuurNet\UiTPASBeheer\ExpenseReport;
+
+use CultuurNet\UiTPASBeheer\Counter\CounterAwareUitpasService;
+use CultuurNet\UiTPASBeheer\Counter\CounterConsumerKey;
+use CultuurNet\UiTPASBeheer\ExpenseReport\Properties\ExpenseReportId;
+use CultuurNet\UiTPASBeheer\Properties\DateRange;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use ValueObjects\Web\Url;
+
+class ExpenseReportService extends CounterAwareUitpasService
+{
+    /**
+     * Route name for downloading an expense report.
+     */
+    const DOWNLOAD_ROUTE_NAME = 'uitpas.expense-report.download';
+
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    /**
+     * @param \CultureFeed_Uitpas $uitpasService
+     * @param CounterConsumerKey $counterConsumerKey
+     * @param UrlGeneratorInterface $urlGenerator
+     */
+    public function __construct(
+        \CultureFeed_Uitpas $uitpasService,
+        CounterConsumerKey $counterConsumerKey,
+        UrlGeneratorInterface $urlGenerator
+    ) {
+        parent::__construct(
+            $uitpasService,
+            $counterConsumerKey
+        );
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    /**
+     * @param DateRange $dateRange
+     * @return ExpenseReportInfo
+     */
+    public function generate(DateRange $dateRange)
+    {
+        $from = $dateRange->getFrom()->toNativeDateTime();
+        $to = $dateRange->getTo()->toNativeDateTime();
+
+        $id = $this->getUitpasService()->generateFinancialOverviewReport(
+            $from,
+            $to,
+            $this->getCounterConsumerKey()
+        );
+
+        return new ExpenseReportInfo(
+            new ExpenseReportId((string) $id),
+            $dateRange
+        );
+    }
+
+    /**
+     * @param ExpenseReportId $id
+     * @return ExpenseReportStatus
+     */
+    public function getStatus(ExpenseReportId $id)
+    {
+        $cfStatus = $this->getUitpasService()->financialOverviewReportStatus(
+            $id->toNative(),
+            $this->getCounterConsumerKey()
+        );
+
+        $completed = $cfStatus->completed();
+        $url = null;
+
+        if ($completed) {
+            $url = $this->urlGenerator->generate(
+                self::DOWNLOAD_ROUTE_NAME,
+                ['expenseReportId' => $id->toNative()],
+                UrlGeneratorInterface::ABSOLUTE_PATH
+            );
+            $url = Url::fromNative($url);
+        }
+
+        $status = new ExpenseReportStatus($completed, $url);
+
+        return $status;
+    }
+}
