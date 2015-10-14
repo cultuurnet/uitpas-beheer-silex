1477c815afb36208e546af433ef7d2624e81025c
Bert Ramakers <bertramakers@me.com>
2015-10-14 16:30:57 +0200

    UBR-308: Added search() method on PassHolderController.

--- src/PassHolder/PassHolderController.php
+++ src/PassHolder/PassHolderController.php
@@ -3,11 +3,19 @@
 namespace CultuurNet\UiTPASBeheer\PassHolder;
 
 use CultuurNet\Deserializer\DeserializerInterface;
+use CultuurNet\Hydra\Symfony\PageUrlGenerator;
+use CultuurNet\UiTPASBeheer\Exception\IncorrectParameterValueException;
 use CultuurNet\UiTPASBeheer\Exception\ReadableCodeResponseException;
+use CultuurNet\UiTPASBeheer\Exception\UnknownParameterException;
+use CultuurNet\UiTPASBeheer\PassHolder\Search\PagedCollection;
+use CultuurNet\UiTPASBeheer\PassHolder\Search\QueryBuilderInterface;
 use CultuurNet\UiTPASBeheer\UiTPAS\UiTPASNumber;
+use CultuurNet\UiTPASBeheer\UiTPAS\UiTPASNumberCollection;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use ValueObjects\Number\Integer;
 use ValueObjects\StringLiteral\StringLiteral;
 
 class PassHolderController
@@ -28,18 +36,119 @@ class PassHolderController
     protected $registrationJsonDeserializer;
 
     /**
+     * @var QueryBuilderInterface
+     */
+    protected $searchQuery;
+
+    /**
+     * @var UrlGeneratorInterface
+     */
+    protected $urlGenerator;
+
+    /**
      * @param PassHolderServiceInterface $passHolderService
      * @param DeserializerInterface $passHolderJsonDeserializer
      * @param DeserializerInterface $registrationJsonDeserializer
+     * @param QueryBuilderInterface $searchQuery
+     * @param UrlGeneratorInterface $urlGenerator
      */
     public function __construct(
         PassHolderServiceInterface $passHolderService,
         DeserializerInterface $passHolderJsonDeserializer,
-        DeserializerInterface $registrationJsonDeserializer
+        DeserializerInterface $registrationJsonDeserializer,
+        QueryBuilderInterface $searchQuery,
+        UrlGeneratorInterface $urlGenerator
     ) {
         $this->passHolderService = $passHolderService;
         $this->passHolderJsonDeserializer = $passHolderJsonDeserializer;
         $this->registrationJsonDeserializer = $registrationJsonDeserializer;
+        $this->searchQuery = $searchQuery;
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     *
+     * @throws UnknownParameterException
+     *   When an unknown query parameter is provided.
+     *
+     * @throws IncorrectParameterValueException
+     *   When a parameter has an invalid value.
+     */
+    public function search(Request $request)
+    {
+        $searchQuery = $this->searchQuery;
+
+        foreach ($request->query->all() as $parameter => $value) {
+            switch ($parameter) {
+                case 'uitpasNumber':
+                    if (!is_array($value)) {
+                        $value = array($value);
+                    }
+
+                    $uitpasNumbers = array_map(
+                        function ($uitpasNumber) {
+                            try {
+                                return new UiTPASNumber($uitpasNumber);
+                            } catch (\InvalidArgumentException $e) {
+                                throw new IncorrectParameterValueException('uitpasNumber');
+                            }
+                        },
+                        $value
+                    );
+
+                    $uitpasNumbers = UiTPASNumberCollection::fromArray($uitpasNumbers);
+
+                    $searchQuery = $searchQuery->withUiTPASNumbers(
+                        $uitpasNumbers
+                    );
+                    break;
+
+                case 'page':
+                case 'limit':
+                    // These are valid but we ignore them for now, we need them
+                    // both in 1 method call.
+                    break;
+
+                default:
+                    throw new UnknownParameterException($parameter);
+            }
+        }
+
+        // Handle both page and limit parameters together.
+        $page = $request->query->getInt('page', 1);
+        $limit = $request->query->getInt('limit', 10);
+
+        $searchQuery = $searchQuery->withPagination(
+            new Integer($page),
+            new Integer($limit)
+        );
+
+        $resultSet = $this->passHolderService->search($searchQuery);
+
+        $pageUrlGenerator = new PageUrlGenerator(
+            $request->query,
+            $this->urlGenerator,
+            $request->attributes->get('_route')
+        );
+
+        $pagedCollection = new PagedCollection(
+            $page,
+            $limit,
+            $resultSet->getResults(),
+            $resultSet->getTotal()->toNative(),
+            $pageUrlGenerator
+        );
+
+        $invalidUitpasNumbers = $resultSet->getInvalidUitpasNumbers();
+        if (!is_null($invalidUitpasNumbers)) {
+            $pagedCollection = $pagedCollection->withInvalidUitpasNumbers($invalidUitpasNumbers);
+        }
+
+        return JsonResponse::create($pagedCollection)
+            ->setPrivate();
     }
 
     /**
