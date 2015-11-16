41780a97d6ab66a7247aca96ffc5934b3d217dc2
Kristof Coomans <kristof@2dotstwice.be>
2015-11-16 21:11:30 +0100

    UBR-372: Remove unneeded additional code to compare JSON

--- test/Coupon/CouponControllerTest.php
+++ test/Coupon/CouponControllerTest.php
@@ -24,36 +24,10 @@ class CouponControllerTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @param string $filePath
-     * @return string
-     */
-    private function getJson($filePath)
-    {
-        return file_get_contents(__DIR__ . '/' . $filePath);
-    }
-
-    /**
-     * @param string $json
-     *   Actual JSON string.
-     * @param string $filePath
-     *   Path to the file with the expected JSON.
-     */
-    private function assertJsonEquals($json, $filePath)
-    {
-        $expected = $this->getJson($filePath);
-        $expected = json_decode($expected);
-
-        $actual = json_decode($json);
-
-        $this->assertEquals($expected, $actual);
-    }
-
-    /**
      * @test
      */
     public function it_responds_the_coupons_for_passholder()
     {
-
         $uitpasNumber = new UiTPASNumber('0930000125607');
 
         $coupons = array();
@@ -72,6 +46,9 @@ class CouponControllerTest extends \PHPUnit_Framework_TestCase
         $response = $this->controller->getCouponsForPassholder($uitpasNumber->toNative());
         $content = $response->getContent();
 
-        $this->assertJsonEquals($content, 'data/coupons.json');
+        $this->assertJsonStringEqualsJsonFile(
+            __DIR__ . '/data/coupons.json',
+            $content
+        );
     }
 }
