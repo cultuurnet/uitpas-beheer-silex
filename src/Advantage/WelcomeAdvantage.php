3a1c8dd89ef79d92388461dfea308026d5b42ad3
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-11-25 10:53:16 +0100

    UBR-359: Adapt WelcomeAdvantage with tests

--- src/Advantage/WelcomeAdvantage.php
+++ src/Advantage/WelcomeAdvantage.php
@@ -2,6 +2,12 @@
 
 namespace CultuurNet\UiTPASBeheer\Advantage;
 
+use CultuurNet\UiTPASBeheer\Properties\City;
+use CultuurNet\UiTPASBeheer\Properties\CityCollection;
+use ValueObjects\DateTime\Date;
+use ValueObjects\DateTime\Month;
+use ValueObjects\DateTime\MonthDay;
+use ValueObjects\DateTime\Year;
 use ValueObjects\Number\Integer;
 use ValueObjects\StringLiteral\StringLiteral;
 
@@ -24,21 +30,57 @@ class WelcomeAdvantage extends Advantage
     }
 
     /**
-     * @param \CultureFeed_Uitpas_Passholder_WelcomeAdvantage $advantage
+     * @param \CultureFeed_Uitpas_Passholder_WelcomeAdvantage $welcomeAdvantage
      * @return static
      */
-    public static function fromCultureFeedWelcomeAdvantage(\CultureFeed_Uitpas_Passholder_WelcomeAdvantage $advantage)
-    {
-        $id = new StringLiteral((string) $advantage->id);
-        $title = new StringLiteral((string) $advantage->title);
+    public static function fromCultureFeedWelcomeAdvantage(
+        \CultureFeed_Uitpas_Passholder_WelcomeAdvantage $welcomeAdvantage
+    ) {
+        $id = new StringLiteral((string) $welcomeAdvantage->id);
+        $title = new StringLiteral((string) $welcomeAdvantage->title);
 
-        $exchangeable = !$advantage->cashedIn &&
-            (is_null($advantage->maxAvailableUnits) || $advantage->maxAvailableUnits > $advantage->unitsTaken);
+        $exchangeable = !$welcomeAdvantage->cashedIn &&
+            (is_null($welcomeAdvantage->maxAvailableUnits) ||
+                $welcomeAdvantage->maxAvailableUnits > $welcomeAdvantage->unitsTaken);
 
-        return new static(
+        $advantage = new static(
             $id,
             $title,
             $exchangeable
         );
+
+        if (!empty($welcomeAdvantage->description1)) {
+            $advantage = $advantage->withDescription1(new StringLiteral($welcomeAdvantage->description1));
+        }
+
+        if (!empty($welcomeAdvantage->description2)) {
+            $advantage = $advantage->withDescription2(new StringLiteral($welcomeAdvantage->description2));
+        }
+
+        if (!empty($welcomeAdvantage->validForCities)) {
+            $cityCollection = new CityCollection();
+
+            foreach ($welcomeAdvantage->validForCities as $city) {
+                $cityCollection = $cityCollection->with(new City($city));
+            }
+
+            $advantage = $advantage->withValidForCities($cityCollection);
+        }
+
+        if (!empty($welcomeAdvantage->counters)) {
+            $advantage = $advantage->withValidForCounters($welcomeAdvantage->counters);
+        }
+
+        if (!empty($welcomeAdvantage->cashingPeriodEnd)) {
+            $dateParts = explode('-', $welcomeAdvantage->cashingPeriodEnd);
+            $dateParts[1] = $dateParts[1] - 1;
+            $advantage = $advantage->withEndDate(new Date(
+                new Year($dateParts[0]),
+                Month::getByOrdinal($dateParts[1]),
+                new MonthDay((int)$dateParts[2])
+            ));
+        }
+
+        return $advantage;
     }
 }
