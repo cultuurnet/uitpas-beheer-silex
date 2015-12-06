6068ff795e246e246787c28cafbd9e883f18b482
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-12-06 20:42:44 +0100

    UBR-401: Add PointsTransactionController with test

---
+++ src/PointsTransaction/PointsTransactionController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace CultuurNet\UiTPASBeheer\PointsTransaction;
+
+use CultuurNet\Clock\Clock;
+use CultuurNet\UiTPASBeheer\UiTPAS\UiTPASNumber;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use ValueObjects\DateTime\Date;
+
+class PointsTransactionController
+{
+    /**
+     * @var PointsTransactionServiceInterface
+     */
+    protected $pointsTransactionService;
+
+    /**
+     * @var Clock
+     */
+    protected $clock;
+
+    /**
+     * PointsTransactionController constructor.
+     * @param PointsTransactionServiceInterface $pointsTransactionService
+     * @param Clock $clock
+     */
+    public function __construct(PointsTransactionServiceInterface $pointsTransactionService, Clock $clock)
+    {
+        $this->pointsTransactionService = $pointsTransactionService;
+        $this->clock = $clock;
+    }
+
+    /**
+     * @param $uitpasNumber
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function getPointsTransactionsForPassholder($uitpasNumber)
+    {
+        $currentTime = $this->clock->getDateTime()->getTimestamp();
+        $startTime = strtotime("-1 year", $currentTime);
+        $endTime = $currentTime;
+
+        $startDate = Date::fromNativeDateTime(
+            \DateTime::createFromFormat('U', $startTime)
+        );
+
+        $endDate = Date::fromNativeDateTime(
+            \DateTime::createFromFormat('U', $endTime)
+        );
+
+        $pointsTransactions = $this->pointsTransactionService->search(
+            new UiTPASNumber($uitpasNumber),
+            $startDate,
+            $endDate
+        );
+
+        return JsonResponse::create()
+            ->setData($pointsTransactions)
+            ->setPrivate(true);
+    }
+}
---
+++ test/PointsTransaction/PointsTransactionControllerTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace CultuurNet\UiTPASBeheer\PointsTransaction;
+
+use CultuurNet\Clock\FrozenClock;
+use CultuurNet\UiTPASBeheer\UiTPAS\UiTPASNumber;
+use DateTimeImmutable;
+use DateTimeZone;
+use ValueObjects\DateTime\Date;
+use ValueObjects\DateTime\Month;
+use ValueObjects\DateTime\MonthDay;
+use ValueObjects\DateTime\Year;
+use ValueObjects\Number\Integer;
+use ValueObjects\StringLiteral\StringLiteral;
+
+class PointsTransactionControllerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var PointsTransactionController
+     */
+    protected $controller;
+
+    /**
+     * @var CombinedPointsTransactionService|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $service;
+
+    /**
+     * @var FrozenClock
+     */
+    protected $clock;
+
+    public function setUp()
+    {
+
+        $now = DateTimeImmutable::createFromFormat('U', '1449237174', new DateTimeZone('Europe/Brussels'));
+        //$now = new DateTimeImmutable('1449237174', new DateTimeZone('Europe/Brussels'));
+        $this->clock = new FrozenClock($now);
+        $this->service = $this->getMock(CombinedPointsTransactionService::class);
+        $this->controller = new PointsTransactionController($this->service, $this->clock);
+    }
+
+    /**
+     * @test
+     */
+    public function it_responds_the_points_transactions_for_passholder()
+    {
+        $uitpasNumber = new UiTPASNumber('0930000125607');
+
+        $currentTime = $this->clock->getDateTime()->getTimestamp();
+        $startTime = strtotime("-1 year", $currentTime);
+        $endTime = $currentTime;
+
+        $startDate = Date::fromNativeDateTime(
+            \DateTime::createFromFormat('U', $startTime)
+        );
+
+        $endDate = Date::fromNativeDateTime(
+            \DateTime::createFromFormat('U', $endTime)
+        );
+
+        $checkin3 = new CheckinPointsTransaction(
+            new Date(
+                new Year(2015),
+                Month::DECEMBER(),
+                new MonthDay(4)
+            ),
+            new StringLiteral('Event 4th of December'),
+            new Integer(10)
+        );
+        $cashedPromotion1 = new CashedPromotionPointsTransaction(
+            new Date(
+                new Year(2015),
+                Month::JUNE(),
+                new MonthDay(23)
+            ),
+            new StringLiteral('Cake for points'),
+            new Integer(15)
+        );
+
+        $checkin2  = new CheckinPointsTransaction(
+            new Date(
+                new Year(2015),
+                Month::MAY(),
+                new MonthDay(13)
+            ),
+            new StringLiteral('Event 13th of May'),
+            new Integer(20)
+        );
+        $checkin1  = new CheckinPointsTransaction(
+            new Date(
+                new Year(2015),
+                Month::JANUARY(),
+                new MonthDay(21)
+            ),
+            new StringLiteral('Event 21st of January'),
+            new Integer(15)
+        );
+
+        $pointsTransactions = [
+            $checkin3,
+            $cashedPromotion1,
+            $checkin2,
+            $checkin1,
+        ];
+
+        $this->service->expects($this->once())
+            ->method('search')
+            ->with($uitpasNumber, $startDate, $endDate)
+            ->willReturn($pointsTransactions);
+
+        $response = $this->controller->getPointsTransactionsForPassholder($uitpasNumber->toNative());
+        $content = $response->getContent();
+
+        $this->assertJsonStringEqualsJsonFile(
+            __DIR__ . '/data/pointsTransactions.json',
+            $content
+        );
+    }
+}
