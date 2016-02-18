36ea5156cae518cc383bbb0228e7036bf0e15a5e
Kristof Coomans <kristof@2dotstwice.be>
2016-02-18 10:57:13 +0100

    UBR-448: Document SchoolServiceInterface methods

--- src/School/SchoolServiceInterface.php
+++ src/School/SchoolServiceInterface.php
@@ -9,7 +9,14 @@ use ValueObjects\StringLiteral\StringLiteral;
 
 interface SchoolServiceInterface
 {
+    /**
+     * @param StringLiteral $uuid
+     * @return School|null
+     */
     public function get(StringLiteral $uuid);
 
+    /**
+     * @return SchoolCollection
+     */
     public function getSchools();
 }
