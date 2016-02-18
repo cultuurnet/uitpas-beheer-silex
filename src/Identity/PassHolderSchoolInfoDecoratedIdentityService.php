6b3a29114bd48bf228dbf4868a80e4d4d2000530
Kristof Coomans <kristof@2dotstwice.be>
2016-02-18 10:41:56 +0100

    UBR-448: Implement adding the school name with the decorator

--- src/Identity/PassHolderSchoolInfoDecoratedIdentityService.php
+++ src/Identity/PassHolderSchoolInfoDecoratedIdentityService.php
@@ -5,6 +5,9 @@
 
 namespace CultuurNet\UiTPASBeheer\Identity;
 
+use CultuurNet\UiTPASBeheer\PassHolder\PassHolder;
+use CultuurNet\UiTPASBeheer\School\SchoolServiceInterface;
+
 class PassHolderSchoolInfoDecoratedIdentityService implements IdentityServiceInterface
 {
     /**
@@ -13,12 +16,19 @@ class PassHolderSchoolInfoDecoratedIdentityService implements IdentityServiceInt
     protected $decoratee;
 
     /**
+     * @var SchoolServiceInterface
+     */
+    protected $schools;
+
+    /**
      * PassHolderSchoolInfoDecoratedIdentityService constructor.
      * @param IdentityServiceInterface $decoratee
+     * @param SchoolServiceInterface $schools
      */
-    public function __construct(IdentityServiceInterface $decoratee)
+    public function __construct(IdentityServiceInterface $decoratee, SchoolServiceInterface $schools)
     {
         $this->decoratee = $decoratee;
+        $this->schools = $schools;
     }
 
     /**
@@ -26,6 +36,21 @@ class PassHolderSchoolInfoDecoratedIdentityService implements IdentityServiceInt
      */
     public function get($identification)
     {
-        return $this->decoratee->get($identification);
+        $identity = $this->decoratee->get($identification);
+
+        if ($identity && $identity->getPassHolder() instanceof PassHolder) {
+            $passHolder = $identity->getPassHolder();
+            $school = $passHolder->getSchool();
+            if ($school) {
+                $enhancedSchool = $this->schools
+                    ->get($school->getId());
+
+                $identity = $identity->withPassHolder(
+                    $passHolder->withSchool($enhancedSchool)
+                );
+            }
+        }
+
+        return $identity;
     }
 }
