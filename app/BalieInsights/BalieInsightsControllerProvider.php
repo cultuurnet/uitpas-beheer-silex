88ac880f5059c07b7c21d7b09d8f74cedae3e8d3
zuuperman <nils.destoop@gmail.com>
2016-07-06 15:50:45 +0200

    Convert BalieInsightsControllerProvider.php also to psr-2 styling

--- app/BalieInsights/BalieInsightsControllerProvider.php
+++ app/BalieInsights/BalieInsightsControllerProvider.php
@@ -17,18 +17,18 @@ class BalieInsightsControllerProvider implements ControllerProviderInterface
         $app['balie_insights_controller'] = $app->share(
             function (Application $app) {
                 return new BalieInsightsController(
-                  $app['balie_insights_service']
+                    $app['balie_insights_service']
                 );
             }
         );
 
-      /* @var ControllerCollection $controllers */
-      $controllers = $app['controllers_factory'];
-      $controllers->get('counters/cardsales', 'balie_insights_controller:getCardSales');
-      $controllers->get('counters/exchanges', 'balie_insights_controller:getExchanges');
-      $controllers->get('counters/mias', 'balie_insights_controller:getMias');
-      $controllers->get('counters/checkins', 'balie_insights_controller:getCheckins');
+        /* @var ControllerCollection $controllers */
+        $controllers = $app['controllers_factory'];
+        $controllers->get('counters/cardsales', 'balie_insights_controller:getCardSales');
+        $controllers->get('counters/exchanges', 'balie_insights_controller:getExchanges');
+        $controllers->get('counters/mias', 'balie_insights_controller:getMias');
+        $controllers->get('counters/checkins', 'balie_insights_controller:getCheckins');
 
-      return $controllers;
+        return $controllers;
     }
 }
