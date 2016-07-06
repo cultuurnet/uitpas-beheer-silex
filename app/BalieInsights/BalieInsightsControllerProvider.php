ac2a2f195944d17763ab95f2c5d2bc778ad4fac4
zuuperman <nils.destoop@gmail.com>
2016-07-06 14:44:47 +0200

    UBR-426: Line indentations

--- app/BalieInsights/BalieInsightsControllerProvider.php
+++ app/BalieInsights/BalieInsightsControllerProvider.php
@@ -12,23 +12,23 @@ use Silex\ControllerProviderInterface;
 
 class BalieInsightsControllerProvider implements ControllerProviderInterface
 {
-  public function connect(Application $app)
-  {
-      $app['balie_insights_controller'] = $app->share(
-          function (Application $app) {
-              return new BalieInsightsController(
-                $app['balie_insights_service']
-              );
-          }
-      );
+    public function connect(Application $app)
+    {
+        $app['balie_insights_controller'] = $app->share(
+            function (Application $app) {
+                return new BalieInsightsController(
+                  $app['balie_insights_service']
+                );
+            }
+        );
 
-    /* @var ControllerCollection $controllers */
-    $controllers = $app['controllers_factory'];
-    $controllers->get('counters/cardsales', 'balie_insights_controller:getCardSales');
-    $controllers->get('counters/exchanges', 'balie_insights_controller:getExchanges');
-    $controllers->get('counters/mias', 'balie_insights_controller:getMias');
-    $controllers->get('counters/checkins', 'balie_insights_controller:getCheckins');
+      /* @var ControllerCollection $controllers */
+      $controllers = $app['controllers_factory'];
+      $controllers->get('counters/cardsales', 'balie_insights_controller:getCardSales');
+      $controllers->get('counters/exchanges', 'balie_insights_controller:getExchanges');
+      $controllers->get('counters/mias', 'balie_insights_controller:getMias');
+      $controllers->get('counters/checkins', 'balie_insights_controller:getCheckins');
 
-    return $controllers;
-  }
+      return $controllers;
+    }
 }
