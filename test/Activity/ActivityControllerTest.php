175175a71e53593eb712d7ae0545846bcff26fea
Thomas Jacobs <thomas.jacobs@wunderkraut.com>
2018-04-04 15:07:46 +0200

    UPS-2471: Fix unit tests

--- test/Activity/ActivityControllerTest.php
+++ test/Activity/ActivityControllerTest.php
@@ -70,7 +70,8 @@ class ActivityControllerTest extends \PHPUnit_Framework_TestCase
             [],
             (new SimpleQuery())
                 ->withPagination(new Integer(1), new Integer(5))
-                ->withUiTPASNumber(new UiTPASNumber('0930000467512')),
+                ->withUiTPASNumber(new UiTPASNumber('0930000467512'))
+                ->withDateRange(new Integer(0), new Integer(0)),
         ];
 
         $items['all possible parameters'] = [
@@ -85,7 +86,8 @@ class ActivityControllerTest extends \PHPUnit_Framework_TestCase
                 ->withDateType(DateType::TODAY())
                 ->withPagination(new Integer(2), new Integer(10))
                 ->withQuery(new StringLiteral('foo'))
-                ->withUiTPASNumber(new UiTPASNumber('0930000467512')),
+                ->withUiTPASNumber(new UiTPASNumber('0930000467512'))
+                ->withDateRange(new Integer(0), new Integer(0)),
         ];
 
         $items['next 12 months'] = [
@@ -100,7 +102,8 @@ class ActivityControllerTest extends \PHPUnit_Framework_TestCase
                 ->withDateType(DateType::NEXT_12_MONTHS())
                 ->withPagination(new Integer(3), new Integer(20))
                 ->withQuery(new StringLiteral('bar'))
-                ->withUiTPASNumber(new UiTPASNumber('0930000208908')),
+                ->withUiTPASNumber(new UiTPASNumber('0930000208908'))
+                ->withDateRange(new Integer(0), new Integer(0)),
 
         ];
 
@@ -114,7 +117,8 @@ class ActivityControllerTest extends \PHPUnit_Framework_TestCase
                 ->withPagination(
                     new Integer(1),
                     new Integer(5)
-                ),
+                )
+                ->withDateRange(new Integer(0), new Integer(0)),
         ];
 
         return $items;
