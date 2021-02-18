01b94b453a4d228c1bb6f90b796db78d19051e4b
Bert Ramakers <bertramakers@me.com>
2021-02-18 17:12:53 +0100

    Add ControllerProvider and ServiceProvider for new Auth classes

---
+++ app/Auth/AuthControllerProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UiTPASBeheer\Auth;
+
+use Auth0\SDK\Auth0;
+use Silex\Application;
+use Silex\ControllerCollection;
+use Silex\ControllerProviderInterface;
+
+final class AuthControllerProvider implements ControllerProviderInterface
+{
+    public function connect(Application $app): ControllerCollection
+    {
+        $app['auth_controller'] = $app->share(function (Application $app) {
+            return new AuthController(
+                $app[Auth0::class],
+                $app['session'],
+                $app['uitid_user_session_service'],
+                $app[UiTIDv1TokenService::class],
+                $app['config']['auth0']['app_url']
+            );
+        });
+
+        $controllers = $app['controllers_factory'];
+
+        $controllers->get('/connect', 'auth_controller:redirectToLoginService');
+        $controllers->get('/authorize', 'auth_controller:storeTokenAndRedirectToFrontend');
+
+        return $controllers;
+    }
+}
