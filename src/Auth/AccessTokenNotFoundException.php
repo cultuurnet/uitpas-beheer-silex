9cc2a48af0462cf0e79358a1a55101cfefc01837
Bert Ramakers <bertramakers@me.com>
2021-02-19 15:47:41 +0100

    Fix coding standards

--- src/Auth/AccessTokenNotFoundException.php
+++ src/Auth/AccessTokenNotFoundException.php
@@ -9,7 +9,8 @@ use Symfony\Component\HttpFoundation\Response;
 
 final class AccessTokenNotFoundException extends ResponseException
 {
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct('No access token found.', Response::HTTP_NOT_FOUND);
     }
 }
