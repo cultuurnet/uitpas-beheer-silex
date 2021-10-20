a92651c143895e09d13437a7d2c85044294cde0a
Bert Ramakers <bertramakers@me.com>
2021-10-20 17:06:55 +0200

    Add new DIGITAL UiTPAS type

--- src/UiTPAS/UiTPASType.php
+++ src/UiTPAS/UiTPASType.php
@@ -8,10 +8,12 @@ use ValueObjects\Enum\Enum;
  * @method static CARD
  * @method static KEY
  * @method static STICKER
+ * @method static DIGITAL
  */
 class UiTPASType extends Enum
 {
     const CARD = 'CARD';
     const KEY = 'KEY';
     const STICKER = 'STICKER';
+    const DIGITAL = 'DIGITAL';
 }
